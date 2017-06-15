0b3b6cdb9ca825b775a60e6da6af5b184ae571df
Klaasjan Horlings <k.horlings@gmail.com>
2017-06-15 03:12:49 +0200

    Initial commit Setup project

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    paths: {
+        source: {
+            server: './source/server'
+        },
+        config: {
+            tsServer: './source/server/tsconfig.json'
+        },
+        dest: {
+            build: './build'
+        }
+    }
+}
